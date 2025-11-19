c59d51f85cf60aa92cd6d79320607598d973a877
bronx999-gif <channelgreenplay@gmail.com>
2025-11-19 07:28:43 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,43 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return 'Hello, World!'
+    return "Number Info API Working 🔥"
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/info", methods=["GET"])
+def info():
+    return jsonify({
+        "success": True,
+        "result": [
+            {
+                "id": 1205627021,
+                "mobile": "7676184626",
+                "name": "Shivalingappa ",
+                "father_name": "Durugappa ",
+                "address": "S/O: Durugappa!257!!harijana colony!nadanga  !Nadanga!Bellary!Karnataka!583114",
+                "alt_mobile": "",
+                "circle": "AIRTEL KARNATKA",
+                "id_number": "",
+                "email": ""
+            },
+            {
+                "id": 1205627022,
+                "mobile": "7676184626",
+                "name": "Shivalingappa",
+                "father_name": "Durugappa",
+                "address": "S/O  Durugappa!!257 harijana colony!nadanga!Nadanga Bellary!Karnataka!583114",
+                "alt_mobile": "919901542381",
+                "circle": "KARNATAKA JIO ",
+                "id_number": "",
+                "email": ""
+            }
+        ],
+        "credit": "@CodeVortexHere"
+    })
+
+
+# for vercel serverless
+def handler(event, context):
+    return app(event, context)
